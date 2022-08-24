3781544d879edadc6502d7d3604fe1ea5a0248d7
AQDev1969 <104890731+AQDev1969@users.noreply.github.com>
2022-08-24 15:21:35 +0200

    Add files via upload

---
+++ assets/QToolbar.7414d1d1.js
@@ -0,0 +1,1 @@
+import{br as s,Q as t,ac as r,bs as n}from"./index.57eaada7.js";var p=s({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:a}){const e=t(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>r("div",{class:e.value},n(a.default))}});export{p as Q};
